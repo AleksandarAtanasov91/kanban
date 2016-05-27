8a626d43f11bb0c3a376673a9318bb6a998d692b
Aleks <aleksandar_atanasov91@abv.bg>
2016-05-27 23:36:56 +0200

    improved component hierarchy

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,28 +1,63 @@
 import uuid from 'node-uuid';
 import React from 'react';
+import Notes from './Notes.jsx';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
+        }
+      ]
+    };
+  }
   render() {
-    const notes = [
-      {
-        id: uuid.v4(),
-        task: 'Learn Webpack'
-      },
-      {
-        id: uuid.v4(),
-        task: 'Learn React'
-      },
-      {
-        id: uuid.v4(),
-        task: 'Do laundry'
-      }
-    ];
+    const notes = this.state.notes;
     return (
       <div>
-        <ul>{notes.map(note =>
-          <li key={note.id}>{note.task}</li>
-        )}</ul>
+        <button onClick={this.addNote}>+</button>
+        <Notes notes={notes} />
       </div>
     );
   }
+  //We are using an experimental feature known as property
+  //initializer here. It allows us to bind the method 'this'
+  //to point at our *App* instance
+  //
+  //Alternatively we could 'bind' at 'constructor' using
+  //a line, such as this.addNote = this.addNote.bind(this);
+  addNote = () => {
+    //It would be possible to write this in an imperative style.
+    //I.e., through 'this.state.notes.push' and then
+    //'this.setState({notes: this.state.notes})' to commit
+    //Alternative solution
+    //addNote = function() {
+    //  this.state.notes.push({
+    //      id: uuid.v4(),
+    //      task: 'New task'
+    //  });
+    //  this.setState({notes: this.state.notes});
+    //}.bind(this);
+
+    //this.setState accepts second parameter like this:
+    //'this.setState({...}, () => console.log('set state!'));'
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New task'
+      }])
+    });
+  };
 }
